2b412ca78c621f99b6bee297dce0ab7afe826e3e
Mathieu Débit <mathieu.debit@gmail.com>
2017-11-03 16:56:33 +0100

    Refactoring: DrawScene with perspective

--- glsl/vertex.vert
+++ glsl/vertex.vert
@@ -1,9 +1,10 @@
 attribute vec2 aPosition;
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
 attribute vec3 aColor;
-
 varying lowp vec3 vColor;
 
 void main() {
-  gl_Position = vec4( aPosition, 0.0, 1.0 );
+  gl_Position = uProjectionMatrix * uModelViewMatrix * vec4( aPosition, 0.0, 1.0 );
   vColor = aColor;
 }
--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@ import NetUtils from './utils/net-utils.js'
 import GlUtils from './utils/gl-utils.js'
 
 
+let rotation = 0.0
+
+
 Promise.all([
   NetUtils.loadText('../glsl/vertex.vert'),
   NetUtils.loadText('../glsl/fragment.frag'),
@@ -35,50 +38,96 @@ function main(resources) {
   const programInfo = {
     program: shaderProgram,
     attribLocations: {
-      position: gl.getAttribLocation(shaderProgram, 'aPosition'),
-      color: gl.getAttribLocation(shaderProgram, 'aColor')
+      vertexPosition: gl.getAttribLocation(shaderProgram, 'aPosition'),
+      vertexColor: gl.getAttribLocation(shaderProgram, 'aColor')
     },
     uniformLocations: {
+      projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
+      modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
     },
   }
 
   const buffers = initBuffers(gl)
 
-  render(gl, programInfo, buffers, canvasInfo)
+  let then = 0
+  function render(now) {
+    now *= 0.001
+    const deltaTime = now - then
+    then = now
+
+    drawScene(gl, programInfo, buffers, canvasInfo, deltaTime)
+
+    requestAnimationFrame(render)
+  }
+  requestAnimationFrame(render)
 }
 
 
 function initBuffers(gl) {
   const vertices = new Float32Array([
-    -1.0, -1.0, 0.0, 1.0, 1.0, // x, y, r, g, b
-     0.0,  0.0, 0.5, 0.0, 1.0,
-     1.0, -1.0, 1.0, 1.0, 0.0,
+   -1.0, -1.0, 1.0, 0.0, 0.0, // x, y, r, g, b
+   -1.0,  1.0, 0.0, 1.0, 0.0,
+    1.0,  1.0, 0.0, 0.0, 1.0,
+    1.0, -1.0, 1.0, 1.0, 1.0,
   ])
-
   const verticesBuffer = gl.createBuffer()
   gl.bindBuffer(gl.ARRAY_BUFFER, verticesBuffer)
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW)
-  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+  const indices = new Uint16Array([
+    0, 1, 2,
+    0, 2, 3,
+  ])
+  const indicesBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indicesBuffer)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW)
 
   return {
     vertices: verticesBuffer,
+    indices: indicesBuffer,
   }
 }
 
 
-function render(gl, programInfo, buffers, canvasInfo) {
-  gl.clearColor(0.13, 0.13, 0.13, 1)
-  gl.clear(gl.COLOR_BUFFER_BIT)
+function drawScene(gl, programInfo, buffers, canvasInfo, deltaTime) {
   gl.viewport(0, 0, canvasInfo.width, canvasInfo.height)
+  gl.clearColor(0.13, 0.13, 0.13, 1)
+  gl.clearDepth(1.0)
+  gl.enable(gl.DEPTH_TEST)
+  gl.depthFunc(gl.LEQUAL)
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
-  gl.useProgram(programInfo.program)
+  const fieldOfView = 45 * Math.PI / 180
+  const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight
+  const zNear = 0.1
+  const zFar = 100.0
+  const projectionMatrix = mat4.create()
+
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar)
+
+  const modelViewMatrix = mat4.create()
+
+  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0])
+  mat4.rotate(modelViewMatrix, modelViewMatrix, rotation, [0, 0, 1])
+  mat4.rotate(modelViewMatrix, modelViewMatrix, rotation * .7, [0, 1, 0])
+
   gl.bindBuffer(gl.ARRAY_BUFFER, buffers.vertices)
 
-  gl.enableVertexAttribArray(programInfo.attribLocations.position)
-  gl.vertexAttribPointer(programInfo.attribLocations.position, 2, gl.FLOAT, false, 20, 0)
+  const { vertexPosition, vertexColor } = programInfo.attribLocations
 
-  gl.enableVertexAttribArray(programInfo.attribLocations.color)
-  gl.vertexAttribPointer(programInfo.attribLocations.color, 3, gl.FLOAT, false, 20, 8 )
+  gl.enableVertexAttribArray(vertexPosition)
+  gl.vertexAttribPointer(vertexPosition, 2, gl.FLOAT, false, 20, 0)
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3)
+  gl.enableVertexAttribArray(vertexColor)
+  gl.vertexAttribPointer(vertexColor, 3, gl.FLOAT, false, 20, 8)
+
+  gl.useProgram(programInfo.program)
+
+  const { uniformLocations } = programInfo
+  gl.uniformMatrix4fv(uniformLocations.projectionMatrix, false, projectionMatrix)
+  gl.uniformMatrix4fv(uniformLocations.modelViewMatrix, false, modelViewMatrix)
+
+  gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_SHORT, 0)
+
+  rotation += deltaTime
 }
